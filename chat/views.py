f3235dd6391e70b52c70d0dfb7211330f537bec5
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-31 16:15:23 +0900

    create ChatRoom class

--- chat/views.py
+++ chat/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = "home.html"
+
+
+class ChatRoom(TemplateView):
+    template_name = "chat/chat_box.html"
+
