8f67e0b1d9dd30ae445be4e10e6a9ee0448d936b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-31 10:27:31 +0900

    create CustomUser class

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+
+class CustomUser(AbstractUser, UserManager):
+    email = models.EmailField(verbose_name="メールアドレス", unique=True, blank=False, null=False)
+    thumbnail = models.ImageField(upload_to="images/thumbnail", verbose_name="サムネイル", blank=True, null=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
