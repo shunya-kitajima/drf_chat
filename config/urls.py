4750506ffbb6964ed110e1db395530c4042a7b17
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-31 15:59:59 +0900

    modify path

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("chat/", include("chat.urls")),
+    path("", include("chat.urls")),
 ]
