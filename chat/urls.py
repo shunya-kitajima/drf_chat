3758205fc4a13f66da902fe405c0e8de32afcea1
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-31 16:15:07 +0900

    include path chat_room

--- chat/urls.py
+++ chat/urls.py
@@ -5,6 +5,7 @@ from chat.views import *
 app_name = "chat"
 
 urlpatterns = [
-    path("", Home.as_view(), name="home")
+    path("", Home.as_view(), name="home"),
+    path("chat_room", ChatRoom.as_view(), name="chat_room")
 ]
 
