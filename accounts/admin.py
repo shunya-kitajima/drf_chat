bec599c672bcea712278a6300b2320299de1c2a5
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-31 10:41:53 +0900

    modify

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext, gettext_lazy
+from django.utils.translation import gettext, gettext_lazy as _
 from .models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
