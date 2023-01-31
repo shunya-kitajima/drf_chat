812668aa4c73184b95db0038afa8952fb41d12e6
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-31 10:41:21 +0900

    import modules

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
 
 # Register your models here.
