886c125cb1ddcdd16e0e9ff6b8ac799d3cc90797
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-31 15:15:09 +0900

    import modules

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
 # Create your views here.
