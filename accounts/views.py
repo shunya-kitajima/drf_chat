944452ad816173b68ab46f6d765afb38c5005204
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-31 15:20:16 +0900

    create SignUp class

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,13 @@ from django.views import generic
 from django.contrib.auth import login
 from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+class SignUp(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("chat:chat_room")
+    template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        login(self.request, self.object)
+        return valid
