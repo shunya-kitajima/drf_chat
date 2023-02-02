a14d4bb4c3384a2bce36f9e34795b0349b2baed7
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-02-03 08:56:45 +0900

    add path

--- chat/urls.py
+++ chat/urls.py
@@ -6,6 +6,7 @@ app_name = "chat"
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
-    path("chat_room/", ChatRoom.as_view(), name="chat_room")
+    path("chat_room/", ChatRoom.as_view(), name="chat_room"),
+    path("search/", SearchUser.as_view(), name="search_user"),
 ]
 
