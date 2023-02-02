21ad443e27326ba041627fd818bbdafd10564cf0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-02-02 16:57:56 +0900

    create FriendAdmin class

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Friends
 
-# Register your models here.
+
+class FriendAdmin(admin, Friends):
+    list_display = ("pk", "user", "friend")
+
+
+admin.site.register(Friends, FriendAdmin)
