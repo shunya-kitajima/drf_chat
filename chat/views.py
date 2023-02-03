d32bb1b3e0ecf7ec4f964b8a7deb9bce2f63fafe
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-02-03 23:49:11 +0900

    modify paramater

--- chat/views.py
+++ chat/views.py
@@ -42,6 +42,7 @@ def get_message(request, username):
             "messages": messages,
             "friends": friends,
             "current_user": current_user,
+            "friend": friend,
         },
     )
 
