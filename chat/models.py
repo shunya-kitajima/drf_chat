e5a447ea046d65f2e7d76745e7c252e987981e8d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-02-03 16:57:25 +0900

    create Messages class

--- chat/models.py
+++ chat/models.py
@@ -21,3 +21,30 @@ class Friends(models.Model):
 
     def __str__(self):
         return f"{self.friend}"
+
+
+class Messages(models.Model):
+    class Meta:
+        ordering = ("timestamp",)
+        verbose_name = "メッセージリスト"
+        verbose_name_plural = "メッセージリスト"
+
+    description = models.TextField()
+    sender_name = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name="送信者",
+        related_name="sender",
+    )
+    receiver_name = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name="受信者",
+        related_name="receiver",
+    )
+    time = models.TimeField(auto_now_add=True)
+    seen = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"To: {self.receiver_name} From: {self.sender_name}"
