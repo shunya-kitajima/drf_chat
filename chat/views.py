5d992b8692cabda8c6118005eba62f054ecc2189
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-02-03 17:20:20 +0900

    create get_message

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.views import View
 from accounts.models import CustomUser
+from .models import Messages
 
 
 def get_friends_list(username):
@@ -14,9 +15,8 @@ def get_friends_list(username):
 
 
 def add_friend(request, username):
-    login_user = request.user.username
     friend = CustomUser.objects.get(username=username)
-    current_user = CustomUser.objects.get(username=login_user)
+    current_user = CustomUser.objects.get(username=request.user.username)
     friend_lists = current_user.user_friends.all()
     flag = 0
     for friend_list in friend_lists:
@@ -27,6 +27,23 @@ def add_friend(request, username):
         current_user.user_friends.create(friend=friend)
         friend.user_friends.create(friend=current_user)
     return redirect("chat:search_user")
+
+
+def get_message(request, username):
+    friend = CustomUser.objects.get(username=username)
+    current_user = CustomUser.objects.get(username=request.user.username)
+    messages = Messages.objects.filter(sender_name=current_user.id, receiver_name=friend.id) | \
+        Messages.objects.filter(sender_name=friend.id, receiver_name=current_user.id)
+    friends = get_friends_list(request.user.username)
+    return render(
+        request,
+        "chat/messages.html",
+        {
+            "messages": messages,
+            "friends": friends,
+            "current_user": current_user,
+        },
+    )
 
 
 class Home(TemplateView):
@@ -54,4 +71,11 @@ class SearchUser(TemplateView):
                     break
 
         friends = get_friends_list(request.user.username)
-        return render(request, "chat/search.html", {"users": user_list, "friends": friends})
+        return render(
+            request,
+            "chat/search.html",
+            {
+                "users": user_list,
+                "friends": friends,
+            },
+        )
