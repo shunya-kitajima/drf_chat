c7896b2faad3f9585a3433b8a40d51f7690fadab
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-02-04 23:58:13 +0900

    create MessageSerializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -3,3 +3,7 @@ from accounts.models import CustomUser
 from chat.models import Messages
 
 
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Messages
+        fields = ["sender_name", "receiver_name", "description", "time"]
